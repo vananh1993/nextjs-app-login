06a8528b3fe9dc838338b8a919270eff3e020157
vananh1993 <vananh.tran@thinkclimax.com>
2024-03-08 18:00:05 +0700

    no message

--- src/app/middleware.ts
+++ src/app/middleware.ts
@@ -5,6 +5,15 @@ import { useLayoutContext } from '@/app/contexts/layoutContext';
 
 
 export  function middeleware(request: NextRequest) {
-    const {layoutState} = useLayoutContext(): any;
+    let {layoutState} = useLayoutContext();
     const isLoggedIN: boolean = layoutState.login_status;
+    let headers = new Headers(request.headers);
+    if(isLoggedIN) {
+        return NextResponse.next();
+    }
+    return NextResponse.redirect(new URL("/", request.url));
+}
+
+export const config = {
+    matcher: ["/Profile"]
 }
